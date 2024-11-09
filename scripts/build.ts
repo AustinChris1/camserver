c04e4e05471eab3e4e5547c934fff98f8af5866a
Adophilus <uchenna19of@gmail.com>
2024-11-09 16:51:59 +0100

    checkpointing...

--- scripts/build.ts
+++ scripts/build.ts
@@ -21,7 +21,7 @@ const functionDirectory = `${buildDir}/functions/index.func`;
 await Bun.build({
 	entrypoints: ["./scripts/run.ts"],
 	outdir: "./build",
-	target: "node"
+	target: "node",
 }).then(console.log);
 
 await $`rm -rf ${buildDir}`;
@@ -35,4 +35,5 @@ await Bun.write(
 	JSON.stringify(functionConfig),
 );
 
-await $`cp ./build/run.js ${functionDirectory}/index.mjs`;
+await $`cp ./build/* ${functionDirectory}`;
+await $`mv ./build/run.js index.mjs`;
